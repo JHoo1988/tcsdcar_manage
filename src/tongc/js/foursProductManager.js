64710cb1cb7ac65c2f47bd803768e82fbd46309a
JHoo1988 <339083113@qq.com>
2017-06-15 20:22:48 +0800

    4S店入口产品管理-获取产品类型选项数据

--- src/tongc/js/foursProductManager.js
+++ src/tongc/js/foursProductManager.js
@@ -43,6 +43,7 @@ layui.use(['jquery', 'simplePager', 'laydate', 'form', 'layer', 'cookie', 'globa
             }
             _self.getData(par);
             _self.bindEvent();
+            _self.getAllProductTypeCategory(par);
         },
 
         bindEvent: function () {
@@ -294,6 +295,64 @@ layui.use(['jquery', 'simplePager', 'laydate', 'form', 'layer', 'cookie', 'globa
                 return false;
             }
             return true;
+        },
+        getAllProductTypeCategory: function (par) {
+            var _self = this;
+            par.pageIndex = 1;
+            par.pageSize = 99999;
+            $.ajax({
+                url: global.url.findAllProductTypeCategory,
+                type: 'GET',
+                dataType: 'json',
+                data: par,
+                success: function (data) {
+                    if (undefined != data.data && null != data.data && data.code == 200) {
+                        var dataList = data.data.content;
+                        var sb = new _self.StringBuffer();
+                        $.each(dataList, function (i, val) {
+                            sb.append("<option value='" + val.id + "'>" + val.name + "</option>");
+                        });
+                        var op = "<option class='brands-option' value='-1'>请选择产品类别</option>";
+                        // $(".brands_search").empty().append(op).append(sb.toString());
+                        $(".brands").empty().append(op).append(sb.toString());
+                        form.render('select');
+                    }
+                }
+            })
+        },
+        StringBuffer: function (str) {
+            var arr = [];
+            str = str || "";
+            var size = 0;  // 存放数组大小
+            arr.push(str);
+            // 追加字符串
+            this.append = function (str1) {
+                arr.push(str1);
+                return this;
+            };
+            // 返回字符串
+            this.toString = function () {
+                return arr.join("");
+            };
+            // 清空
+            this.clear = function (key) {
+                size = 0;
+                arr = [];
+            }
+            // 返回数组大小
+            this.size = function () {
+                return size;
+            }
+            // 返回数组
+            this.toArray = function () {
+                return buffer;
+            }
+            // 倒序返回字符串
+            this.doReverse = function () {
+                var str = buffer.join('');
+                str = str.split('');
+                return str.reverse().join('');
+            }
         }
     };
     var page = new Page();
